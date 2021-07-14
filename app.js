0013341f10dc0eecf32cb8d87a8dfbfb35744626
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-07-14 16:20:01 -0700

    final

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const nameEl = document.getElementById('name');
 
 const buttonEl = document.getElementById('name-button')
 
+const pinkButtonEl = document.getElementById('pink-button')
+
+
+
 
 buttonEl.addEventListener('click', () => {
     const inputEl = document.getElementById('name-input')
     nameEl.textContent = inputEl.value;
 })
+
