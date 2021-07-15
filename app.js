20571d0b1506024f6da59fd2a4ca8a45f2b7bf71
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-07-14 17:14:26 -0700

    added pronouns

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const nameEl = document.getElementById('name');
 
+const pronounEl = document.getElementById('pronoun');
+
 const buttonEl = document.getElementById('name-button')
 
 const pinkButtonEl = document.getElementById('pink-button')
@@ -9,6 +11,8 @@ const pinkButtonEl = document.getElementById('pink-button')
 
 buttonEl.addEventListener('click', () => {         
     const inputEl = document.getElementById('name-input')
+    const pronounInputEl = document.getElementById('pronoun-input')
     nameEl.textContent = inputEl.value;
+    pronounEl.textContent = pronounInputEl.value;
 })
 
